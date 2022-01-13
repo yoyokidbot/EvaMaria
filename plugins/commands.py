f98536eb40682442e9eaa7d1bc5dd8351428a7b4
pijiomatter <97387090+pijiomatter@users.noreply.github.com>
2022-01-13 11:04:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -84,11 +84,12 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🔍 Search 🔎', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('👨‍💻 Owner 👨‍💻', url='https://t.me/stephinsabu')
+
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ Help ℹ', callback_data='help'),
+            InlineKeyboardButton('🌟 About ⚡️', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
